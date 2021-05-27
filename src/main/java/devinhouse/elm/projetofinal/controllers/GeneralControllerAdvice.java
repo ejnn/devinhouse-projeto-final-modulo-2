942d1e00357fa918584810516386025a8edb98bc
Milena Diniz Vieira <miladvieira@outlook.com>
2021-05-27 20:09:36 -0300

    Tratando exceção nos controllers

--- src/main/java/devinhouse/elm/projetofinal/controllers/GeneralControllerAdvice.java
+++ src/main/java/devinhouse/elm/projetofinal/controllers/GeneralControllerAdvice.java
@@ -2,6 +2,11 @@ package devinhouse.elm.projetofinal.controllers;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import devinhouse.elm.projetofinal.exceptions.IdJaExisteException;
+import devinhouse.elm.projetofinal.exceptions.IdentificacaoInvalidaException;
+import devinhouse.elm.projetofinal.exceptions.IdentificacaoJaExisteException;
+
 import org.hibernate.PropertyValueException;
 import org.springframework.http.ResponseEntity;
 import static org.springframework.http.HttpStatus.*;
@@ -17,8 +22,24 @@ public class GeneralControllerAdvice {
     }
 
     @ExceptionHandler
+    public ResponseEntity<String> handleIdentificacaoInvalidaException(IdentificacaoInvalidaException exception) {
+        return new ResponseEntity<>(exception.getMessage(), exception.getStatus());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleIdJaExisteException(IdJaExisteException exception) {
+        return new ResponseEntity<>(exception.getMessage(), exception.getStatus());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleIdentificacaoJaExisteException(IdentificacaoJaExisteException exception) {
+        return new ResponseEntity<>(exception.getMessage(), exception.getStatus());
+    }
+
+    @ExceptionHandler
     public ResponseEntity<String> handlePropertyValueException(PropertyValueException exception) {
         return new ResponseEntity<>(
-            "Erro na propriedade \"" + exception.getPropertyName() + "\" (" + exception.getCause() + ").", BAD_REQUEST);
+                "Erro na propriedade \"" + exception.getPropertyName() + "\" (" + exception.getCause() + ").",
+                BAD_REQUEST);
     }
 }
